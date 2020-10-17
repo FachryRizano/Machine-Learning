a3f50b34e88e1a371ddb6fdacf8f1c715c3d1570
FachryRizano <fachry.rizano@gmail.com>
2020-10-17 09:44:12 +0700

    mengganti file data

--- Seaborne Exercise/Exercise.py
+++ Seaborne Exercise/Exercise.py
@@ -2,6 +2,13 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 sns.set_style('whitegrid')
-df = pd.read_csv('train.csv')
-sns.jointplot(x='Fare',y='Age',data=df)
+titanic = pd.read_csv('titanic.csv')
+#Question 1
+#sns.jointplot(x='fare',y='age',data=titanic)
+#Question 2
+# a = sns.distplot(titanic['fare'],kde=False,color='salmon')
+# a.set(xlim=(0,600))
+# a.set(ylim=(0,500))
+#Question 3
+print(titanic.info())
 plt.show()
